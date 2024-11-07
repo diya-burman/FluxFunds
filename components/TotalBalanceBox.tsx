53a153e8af7d369af010d786794027678f0be9c4
Diya Burman <diyaburman60@gmail.com>
2024-11-08 00:58:12 +0530

    Third Commit

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,7 @@
+import AnimatedCounter from './AnimatedCounter';
+import { formatAmount } from '@/lib/utils'
 import React from 'react'
+import CountUp from 'react-countup'
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -11,8 +14,19 @@ const TotalBalanceBox = ({
 
             <div className="flex flex-col gap-6">
               <h2 className="header-2">
-                {totalBanks} Bank Accounts
+              Bank Accounts: {totalBanks} 
               </h2>
+              <div className="flex flex-col gap-2">
+                <p className="total-balance-label">
+                  Total Current Balance
+                </p>
+
+                <p className="total-balance-amount flex-center gap-2">
+                  <AnimatedCounter amount=
+                  {totalCurrentBalance}/>
+                  
+                </p>
+              </div>
             </div>
         </section>
     )
