e5262246293e6a86ba87096c0ae784f2fc34d428
Diya Burman <diyaburman60@gmail.com>
2024-11-08 23:25:51 +0530

    Commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,12 @@
-import { Sidebar } from "lucide-react";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {firstName: 'Diya', lastName: 'Burman'};
   return (
-    <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn}/>
-        {children}
+    <main>
+      SIDEBAR
+      {children}
     </main>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,46 +1,59 @@
-"use client"
-import { sidebarLinks } from '@/constants';
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
+'use client'
 
-const Sidebar = ({user}: SiderbarProps) => {
-    const pathname = usePathname();
-    return (
-        <section className="sidebar">
-            <nav className="flex flex-col gap-4">
-              <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
-                <img
-                  src="/icons/logo.svg"
-                  width={34}
-                  height={34}
-                  alt="FluxFunds logo"
-                  className="size=[24px] max-xl:size-14"
+import { sidebarLinks } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
+
+  return (
+    <section className="sidebar">
+      <nav className="flex flex-col gap-4">
+        <Link href="/" className="mb-12 cursor-pointer flex items-center gap-2">
+          <Image 
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">FluxFunds</h1>
+        </Link>
+
+        {sidebarLinks.map((item) => {
+          const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+
+          return (
+            <Link href={item.route} key={item.label}
+              className={cn('sidebar-link', { 'bg-bank-gradient': isActive })}
+            >
+              <div className="relative size-6">
+                <Image 
+                  src={item.imgURL}
+                  alt={item.label}
+                  fill
+                  className={cn({
+                    'brightness-[3] invert-0': isActive
+                  })}
                 />
-                <h1 className="sidebar-logo">FluxFunds</h1>
-              </Link>
+              </div>
+              <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                {item.label}
+              </p>
+            </Link>
+          )
+        })}
+        
+       
+      </nav>
 
-              {sidebarLinks.map((item) => {
-                const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
-                return (
-                    <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bd-bank-gradient': isActive })}>
-                      <div className="relative size-6">
-                        <img 
-                          src={item.imgURL}
-                          alt={item.label}
-                          className={cn({'brightness-[3] invert-0':isActive})}
-                        />
-                      </div>
-                      <p className={cn("sidebar-label",{"!text-white": isActive})}>
-                        {item.label}
-                      </p>
-                    </Link>
-                )
-              })}
-            </nav>
-        </section>
-    )
+    
+    </section>
+  )
 }
 
 export default Sidebar
