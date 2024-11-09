151d61785d6e118f540032fe379797cb2e640a13
Diya Burman <diyaburman60@gmail.com>
2024-11-09 17:14:14 +0530

    commit changes to root  Page

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,3 +1,4 @@
+import { formatAmount } from "@/lib/utils";
 import Link from "next/link";
 import React from "react";
 
@@ -11,7 +12,12 @@ const BankCard = ({
       <Link href="/" className="bank-card">
         <div className="bank-card_content">
             <div>
-                <h1 className="text-16 font-semibold text-white"></h1>
+                <h1 className="text-16 font-semibold text-white">
+                    {account.name || userName }
+                </h1>
+                <p className="font-ibm-plex-serif font-black text-white">
+                    {formatAmount(account.currentBalance)}
+                </p>
             </div>
         </div>
       </Link>
